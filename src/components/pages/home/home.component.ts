7bff2386f3507637dd571335ef2d9856abc61d8e
ThatGhost <stargamer.me@gmail.com>
2024-02-24 16:37:25 +0100

    added gamedevelopment section and social media buttons

--- src/components/pages/home/home.component.ts
+++ src/components/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { MainComponent } from '../../standalone/main/main.component';
 import { AboutMeComponent } from '../../standalone/about-me/about-me.component';
 import { ContactComponent } from '../../standalone/contact/contact.component';
 import { WebDevelopmentComponent } from '../../standalone/web-development/web-development.component';
+import { GameDevelopmentComponent } from '../../standalone/game-development/game-development.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainComponent, AboutMeComponent, ContactComponent, WebDevelopmentComponent],
+  imports: [MainComponent, AboutMeComponent, ContactComponent, WebDevelopmentComponent, GameDevelopmentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/components/standalone/game-development/game-development.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-development',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-development.component.html',
+  styleUrl: './game-development.component.scss'
+})
+export class GameDevelopmentComponent {
+
+}
