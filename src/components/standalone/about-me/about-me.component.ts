822690c673c2f2aa65667f5443b0d1c319024a4e
ThatGhost <stargamer.me@gmail.com>
2024-02-26 13:09:41 +0100

    added feedback

--- src/components/standalone/about-me/about-me.component.ts
+++ src/components/standalone/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  @ViewChild('aboutme') aboutme!: ElementRef<HTMLSelectElement>;
+  @ViewChildren("card") cards!: QueryList<ElementRef>;
 
+  private hasScrolled: boolean = false;
+
+  constructor() {
+    document.addEventListener('scroll', (e) => {
+      this.updateOnScroll(e.target);
+    })
+  }
+
+  private updateOnScroll(e: any) {
+    if(this.hasScrolled === true) return;
+
+    const offsetY: number = this.aboutme.nativeElement.offsetTop;
+    const scrollY: number = window.scrollY;
+
+    if(scrollY >= offsetY) {
+      for(const card of this.cards) {
+        card.nativeElement.classList.remove("fulltop");
+      }
+      this.hasScrolled = true;
+      document.removeEventListener('scroll', () => {});
+    }
+  }
 }
